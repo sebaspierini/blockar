24ff28bc0a05a3870d685fa5ae74a82c21ab31b0
sebaspierini <seba.i.pierini@gmail.com>
2020-02-28 17:53:58 -0300

    se borran archivos innecesarios

--- actions_sprite.js
+++
@@ -1,5 +0,0 @@
-function collectStar (sprite, star)
-{
-    star.disableBody(true, true);
-    console.log("choca con star");
-}
--- preload_game.js
+++
@@ -1,9 +0,0 @@
-function preload ()
-{
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('play', 'assets/play.png');
-    this.load.image('reset', 'assets/reset.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-    this.load.html('html', 'prueba.html');
-}
